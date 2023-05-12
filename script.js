ade5f313638c55f50eb9d5301889654d89660d88
Nix7amcm <65091582+Nix7amcm@users.noreply.github.com>
2023-05-12 21:03:09 +0100

    toggle accordion item
    - deleted h2 container from button
    - open class added to collapse, display block
    - added collapsing styles
    - added toggle accordion item logic (with notes)

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log( 'test' );
+document.querySelectorAll( '.accordion__question' ).forEach( ( item ) => {
+  item.addEventListener( 'click', ( event ) => {
+
+    //### OPEN accordian item ###//
+    //--- 1-  If '.accordion__question' NO(!) class 'open':
+    if ( !item.classList.contains( 'open' ) ) {                              // 1
+      //::::: ( ...then clicked: )
+      //--- 2-  (-) 'collapse' from '.accordion__collapse' (sibling) &
+      //---     (+) 'collapsing' class:
+      item.nextElementSibling.classList = 'accordion__collapse collapsing';  // 2
+      //--- 3-  After X time:
+      setTimeout( () => {                                                    // 3
+        //::::: ( ...open accordion by: )
+        //--- 4-  (-) 'collapsing' class &
+        //---     (+) 'open' class:
+        item.nextElementSibling.classList = 'accordion__collapse open';      // 4
+      }, 300 );                                                              // 3
+
+    }
+
+    //### CLOSE accordion item ###//
+    //--- 5-  If '.accordion__question' HAS class 'open':
+    else {                                                                   // 5
+      //::::: ( ...then clicked: )
+      //--- 6-  (-) 'open' from '.accordion__collapse' (sibling) &
+      //---     (+) 'collapsing' class:
+      item.nextElementSibling.classList = 'accordion__collapse collapsing';  // 6
+      //--- 7-  After X time: 
+      setTimeout( () => {                                                    // 7
+        //::::: ( ...close accordion by: )
+        //--- 8-  (-) 'collapsing' class & 
+        //---     (+) 'collapse' class:
+        item.nextElementSibling.classList = 'accordion__collapse collapse';  // 8
+      }, 300 );                                                              // 7
+
+    }
+
+    item.classList.toggle( 'open' );
+
+  } );
+} );
